b781c46dd999e133d6c237bdc21c3693ed791c3f
chetanya1998 <chetanyaved@gmail.com>
2020-01-22 13:50:31 +0530

    blockchain file

---
+++ block.py
@@ -0,0 +1,45 @@
+import time
+
+from crypto_hash import crypto_hash
+class Block:
+    """
+    block : a unit of storage
+    store transactions in a blockchain that supports a cryptocurrency
+    """
+    def __init__(self,timestamp,data,last_hash,hash):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
+        self.data = data
+
+    def __repr__(self):
+        return(
+           'Block('
+           f'timestamp:{self.timestamp},'
+           f'last_hash:{self.last_hash},'
+           f'hash:{self.hash},'
+           f'data:{self.data})'
+        ) 
+    @staticmethod
+    def mine_block(last_block,data):
+        """
+        mine a block based on the given last block and data.
+        """
+        timestamp = time.time()
+        last_hash = last_block.hash
+        hash = crypto_hash(timestamp,last_hash,data)
+        return Block(timestamp,last_hash,hash,data)
+    @staticmethod
+    def genesis():
+        """
+        Generate the genesis block.
+         """
+        return Block(1,'genesis_last_hash','genesis_hash',[])
+
+def main():
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block,'foo')
+    print(block)
+
+if __name__== '__main__' :
+     main()
--- blockchain.py
+++ blockchain.py
@@ -1,27 +1,24 @@
-def lightning_hash(data):
-    return data +'*'
-class Block:
-  def __init__(self,data,hash,last_hash):
-    self.data = data
-    self.hash = hash
-    self.last_hash = last_hash
+from block import Block
 
 class Blockchain:
-  def __init__(self):
-    genesis = Block('gen_data','gen_hash','gen_last_hash')
+    """
+    Blockchain:A public ledger of transactions
+    Implemented as a list of blocks - data sets of transactions
 
+    """
+    def __init__(self):
+        self.chain = [Block.genesis()]
+    def add_block(self,data):
+        
+        self.chain.append(Block.mine_block(self.chain[-1],data))
+    def __repr__(self):
+        return f'Blockchain:{self.chain}'
+def main():
+        blockchain = Blockchain()
+        blockchain.add_block('one')
+        blockchain.add_block('two')
 
-    self.chain=[genesis]
-  def add_block(self,data):
-   last_hash=self.chain[-1].hash
-   hash =lightning_hash(data+last_hash)
-   block = Block(data,hash,last_hash)
-
-   self.chain.append(block)
-foo_blockchain = Blockchain()
-foo_blockchain.add_block('one')
-foo_blockchain.add_block('two')
-foo_blockchain.add_block('three')
-
-for block in foo_blockchain.chain:
-  print(block.__dict__)
+        print(blockchain)
+        print(f'blockchain.py__name__:{__name__}')
+if __name__ == '__main__':
+        main()
